a843a783e727e3e7a310ae69bcd6c4830c3a51cb
isJaakko <1512803764@qq.com>
2025-03-19 00:03:34 +0800

    feat: 还原代码

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-// import records from './records/start_game.json';
-// import { translateAction, wait } from './translate/index.js';
-
-export const wait = timout => new Promise(resolve => setTimeout(() => resolve(), timout))
+import records from './records/start_game.json';
+import { translateAction, wait } from './translate/index.js';
 
 async function main() {
-    console.log('start');
-    await wait(5000);
-    console.log('end');
-    // const { actions } = records;
+    const { actions } = records;
 
-    // for (let index = 0; index < actions.length; index++) {
-    //     const action = actions[index];
-    //     await translateAction(action, index);
-    // }
+    for (let index = 0; index < actions.length; index++) {
+        const action = actions[index];
+        await translateAction(action, index);
+    }
 }
 
 main();
